b97b6e22c3f974715e975c9ebbaaac46e3e6da2a
Vladster1256 <vcmariorocks@gmail.com>
2014-11-14 13:45:16 -0700

    asdf
    asdf

---
+++ gen/ctec/tehsuperawesomeandroidproject/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ctec.tehsuperawesomeandroidproject;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/ctec/tehsuperawesomeandroidproject/MainActivity.java
@@ -0,0 +1,57 @@
+package ctec.tehsuperawesomeandroidproject;
+
+import android.app.Activity;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+public class MainActivity extends Activity
+{
+
+	private Switch tehButton;
+	private ImageView DogeFace;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		DogeFace = (ImageView) findViewById(R.id.imageView1);
+		tehButton = (Switch) findViewById(R.id.button1);
+
+		setupListeners();
+	}
+
+	private void setupListeners()
+	{
+		// TODO Auto-generated method 
+		tehButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void  onClick(View currentView)
+			{
+				try{
+					if(true)
+					{
+						DogeFace.setVisibility(View.INVISIBLE);
+					}
+					else
+					{
+						DogeFace.setVisibility(View.VISIBLE);
+				
+					}
+				}	
+				catch(NullPointerException)
+				{
+					
+				}
+			}
+		});
+	}
+}
